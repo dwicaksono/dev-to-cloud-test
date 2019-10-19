863e84fdd3c44a611a40c66b6dd13e1650d5838c
dimas wicaksono <novan.top@gmail.com>
2019-10-20 03:31:09 +0700

    :test:

--- introToExpress/app.js
+++ introToExpress/app.js
@@ -3,9 +3,11 @@ let app = express();
 let port = 3000;
 
 // get '/' =>> hello
+// app.get("*", (reg, res) => res.send("HOME"));
 app.get("/", (reg, res) => res.send("hello"));
 app.get("/kucing", (reg, res) => res.send("MEOW..MEOW"));
 app.get("/oi", (reg, res) => res.send("oi..oi..oi"));
+app.get("/oi/:suboi", (reg, res) => res.send("sub..oi..oi"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 // app.listen(port);
